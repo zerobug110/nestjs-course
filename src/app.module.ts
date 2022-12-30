3d33fe9061ff7dd2a532ce2e71fe039fccce9850
zerobug110 <mahabojang110@gmail.com>
2022-12-30 15:02:47 +0000

    refactore the nest files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule]
+  imports: [TaskModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-
-@Module({
-    imports: []
-})
-
-export class AuthModule {
-
-}
