f0c3294d3cbd7808e68c067359a7ca37571e22a8
zerobug110 <mahabojang110@gmail.com>
2023-01-01 01:45:01 +0000

    added task model

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,9 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './task.modal';
 import { TaskService } from './task.service';
 @Controller('task')
 export class TaskController {
   constructor(private taskservice: TaskService) {
 
+  }
+
+  @Get()
+  getAllTask(): Task[] {
+    return this.taskservice.getAllTask()
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.modal';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  private tasks: Task[] = [];
+
+  getAllTask(): Task[] {
+    return this.tasks;
+  }
+}
